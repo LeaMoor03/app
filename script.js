c996a482654026d8048652320ac1dc1d1d2d8e1f
Leah Moore <leahmoore733@gmail.com>
2023-07-21 02:37:44 -0500

    Added the current time

---
+++ script.js
@@ -0,0 +1,13 @@
+let date = document.querySelector("#date");
+let currentTime = new Date();
+
+let hour = currentTime.getHours();
+let minute = currentTime.getMinutes();
+
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = weekDays[currentTime.getDay()];
+
+date.innerHTML = `${day} ${hour}:${minute}`;
+
+
+
