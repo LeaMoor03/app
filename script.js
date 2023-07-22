5572ff4f48aa541236b22e9617501ef44f6e1f4b
Leah Moore <leahmoore733@gmail.com>
2023-07-22 03:49:14 -0500

    Replaced the div row

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let currentTime = new Date();
 let hour = currentTime.getHours();
 let minute = currentTime.getMinutes();
 
+
 let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let day = weekDays[currentTime.getDay()];
 
