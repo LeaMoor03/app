f7145c16c02eb4d88acc9e3434733f4ede1985f6
Leah Moore <leahmoore733@gmail.com>
2023-07-22 18:31:50 -0500

    Updated the time

--- script.js
+++ script.js
@@ -11,13 +11,31 @@ let day = weekDays[currentTime.getDay()];
 date.innerHTML = `${day} ${hour}:${minute}`;
 
 
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (hours <10) {
+    hours = `0${hours}`;
+}
+if (minutes <10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+
+
   function displayTemperature(response) {
     console.log(response.data);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#current-forcast").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML =Math.round(response.data.wind.speed);
+    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
   }
 
 
